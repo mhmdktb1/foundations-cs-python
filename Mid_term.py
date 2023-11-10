93781c629d75b2f477dd28ac78082a2e3c6c7a89
mhmd <kotob149@gmail.com>
2023-11-10 19:26:50 +0200

    display menu

---
+++ Mid_term.py
@@ -0,0 +1,11 @@
+def display_menu():
+    print("Welcome to the Tab Manager!")
+    print("1. Open Tab")
+    print("2. Close Tab")
+    print("3. Switch Tab")
+    print("4. Display All Tabs")
+    print("5. Open Nested Tab")
+    print("6. Clear All Tabs")
+    print("7. Save Tabs")
+    print("8. Import Tabs")
+    print("9. Exit")
