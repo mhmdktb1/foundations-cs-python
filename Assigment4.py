7d4a2acbc61c29afceaecd65ba3591c619a28af4
mhmd <kotob149@gmail.com>
2023-11-21 14:33:01 +0200

    add to linked list

--- Assigment4.py
+++ Assigment4.py
@@ -2,3 +2,19 @@ class Node():
   def __init__(self,value):
     self.value=value
     self.next=None
+class Linkedlist():
+  def __init__(self):
+    self.size=0
+    self.head=None
+    self.tail=None
+  def addnode(self,value):
+    node=Node(value)
+    if self.size ==0: 
+      self.head=node
+      self.tail=node   
+      self.size+=1
+    else:
+      self.tail.next=node
+      self.tail=node
+      self.size+=1
+ s
