12837b4286678bd0eb9f78746cd768914f41734e
mhmd <kotob149@gmail.com>
2023-11-25 11:04:10 +0200

    enque

--- Assigment4.py
+++ Assigment4.py
@@ -65,6 +65,51 @@ class stack():
       poped=self.items.pop()
       self.size-=1
       return poped 
+   def clear(self):
+      self.items.clear()
+class Queue():
+   def __init__(self):
+      self.head=None
+      self.rear=None
+      self.size=0
+   def display(self):
+        current = self.head
+        while current:
+            print(current.value, end=" ")
+            current = current.next
+        print()
+
+   def enqueue(self,value):
+       
+       node=Node(value)
+       if self.size==0:
+        self.head=node
+        self.rear=node
+        self.size+=1
+        print("Added to the Q:",value )
+       else:
+          self.rear.next=node
+          self.rear=node
+          self.size+=1
+          print("Added to the Q:",value )
+
+   def dequeu(self):
+    if self.size == 0:
+        print("Your Queue is Empty! Enqueue first.")
+    elif self.size == 1:
+        print("We are removing:", self.head.value)
+        self.head = None
+        self.size -= 1
+    else:
+        if self.head is not None:
+            print("We are removing:", self.head.value)
+            current = self.head
+            self.head = self.head.next
+            current.next = None
+            self.size -= 1
+        else:
+            print("Unexpected error: self.head is None.")
+
 def ispalindrome():
    list1=[]
    st=stack()
@@ -83,4 +128,5 @@ def ispalindrome():
          print('palindrome')
       else:
          print("Not palindrome")
-ispalindrome()
+   st.clear
+   list1.clear   
